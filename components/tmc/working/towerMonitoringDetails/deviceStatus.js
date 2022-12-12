1b19a379bdab7365bb30dd052e9942f7a76bb4b7
Pardeep Bhardwaj <pardeepbhardwaj10@mail.com>
2022-12-12 20:00:50 +0530

    nokia mom 12122022

--- components/tmc/working/towerMonitoringDetails/deviceStatus.js
+++ components/tmc/working/towerMonitoringDetails/deviceStatus.js
@@ -53,7 +53,7 @@ class DeviceBatteryStatusDetails extends Wrapper {
                             Child 2 Connectivity <br />With Device
                         </th>
                         <th>
-                            Height
+                            Height (M)
                         </th>
                         <th>
                             Height Status
