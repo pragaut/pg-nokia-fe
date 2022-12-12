efd3647a2d376590a92dc4c2debc5c9ec221f3ca
Pardeep Bhardwaj <pardeepbhardwaj10@mail.com>
2022-12-12 18:45:17 +0530

    nokia mom 12122022

--- components/tmc/working/towerMonitoringDetails/towerMonitoringDetails.js
+++ components/tmc/working/towerMonitoringDetails/towerMonitoringDetails.js
@@ -148,7 +148,7 @@ class TowerMonitoringDetailedIndex extends Wrapper {
                             <i class="fa fa-refresh" aria-hidden="true"></i>
                         </Button>
                         <Button
-                            width="50px"
+                            width="150px"
                             height="30px"
                             borderRadius="5px"
                             bgColor="blue"
